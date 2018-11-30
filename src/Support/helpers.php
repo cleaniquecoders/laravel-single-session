09edfee21a6596794ee7471a59130423bb7bad95
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-11-30 10:59:31 +0800

    Added Helper

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -1,1 +1,8 @@
 <?php
+
+if (! function_exists('singleSession')) {
+    function singleSession(string $identifier)
+    {
+        return \CleaniqueCoders\LaravelSingleSession\Services\SingleSession::make($identifier);
+    }
+}
